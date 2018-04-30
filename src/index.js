5c4d47c53efb3cad75d8d2d03ee177fbf0c0a6d4
Nicolas Favaro <nic@dojomojo.ninja>
2018-04-30 17:04:14 -0400

    withForm wrapper component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { Form } from './form/form'
+export { withForm } from './form/withForm'
 export { InputWrapper, SubmitWrapper } from './form/input-wrapper'
 export { Input } from './form/input'
 export * from './form/select'
